b732520977d09d2a69e8b4e7d105cb5134267b12
Mauri <mauri.monzon91@gmail.com>
2023-08-14 16:31:40 -0300

    Dimensiones con porcentaje en el style

--- src/screens/DimensionesScreen.tsx
+++ src/screens/DimensionesScreen.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, useWindowDimensions} from 'react-native';
 
-const {width, height} = Dimensions.get('window');
+/* const {width, height} = Dimensions.get('window'); */
 
 export const DimensionesScreen = () => {
+  const {width, height} = useWindowDimensions();
   return (
     <View>
       <View style={styles.container}>
-        <View style={styles.cajaMorada} />
+        <View style={{...styles.cajaMorada, width: width * 0.5}} />
         <View style={styles.cajaNaranja} />
       </View>
       <Text style={styles.title}>W: {width}</Text>
@@ -24,7 +25,7 @@ const styles = StyleSheet.create({
   },
   cajaMorada: {
     backgroundColor: '#5856D6',
-    width: '50%',
+    /*     width: '50%', */
     height: '50%',
   },
   cajaNaranja: {
