71f99641621410326c3a25c82b04bcba3ca0b953
Mauri <mauri.monzon91@gmail.com>
2023-08-14 09:30:46 -0300

    Screens

--- App.tsx
+++ App.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
 
 const App = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text
-        style={{
-          fontSize: 45,
-          textAlign: 'center',
-        }}>
-        Hola mundo!!!
-      </Text>
-    </View>
-  );
+  return <HolaMundoScreen />;
 };
 
 export default App;
