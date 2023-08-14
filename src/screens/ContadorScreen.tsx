d1193a1dab9dbc811eb375759189e1bacfd83068
Mauri <mauri.monzon91@gmail.com>
2023-08-14 10:28:33 -0300

    Botones flotantes de suma y resta

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -7,9 +7,18 @@ export const ContadorScreen = () => {
     <View style={styles.container}>
       <Text style={styles.tittle}>Contador: {contador}</Text>
       {/*     <Button title="Click" onPress={() => setContador(contador + 1)} /> */}
-      <TouchableOpacity onPress={() => setContador(contador + 1)}>
-        <View style={styles.buttonIncrement}>
-          <Text>+1</Text>
+      <TouchableOpacity
+        style={styles.fabLocationBR}
+        onPress={() => setContador(contador + 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.fabLocationBL}
+        onPress={() => setContador(contador - 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -22,12 +31,31 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   tittle: {
-    textAlign: 'center',
     fontSize: 40,
+    textAlign: 'center',
     top: -15,
   },
-  buttonIncrement: {
-    backgroundColor: 'red',
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 25,
+    left: 25,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
     borderRadius: 100,
+    justifyContent: 'center',
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    alignSelf: 'center',
   },
 });
