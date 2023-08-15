91949e3517666ecd2f84c8b797c234a6b864c5b3
Mauri <mauri.monzon91@gmail.com>
2023-08-15 10:43:09 -0300

    Componente para Tarea

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import {TareaScreen} from './src/screens/TareaScreen';
 /* import {HolaMundoScreen} from './src/screens/HolaMundoScreen'; */
 /* import {ContadorScreen} from './src/screens/ContadorScreen'; */
 /* import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen'; */
 /* import {DimensionesScreen} from './src/screens/DimensionesScreen'; */
 /* import {PositionScreen} from './src/screens/PositionScreen'; */
-import {FlexScreen} from './src/screens/FlexScreen';
+/* import {FlexScreen} from './src/screens/FlexScreen'; */
 
 const App = () => {
   return (
@@ -15,7 +16,8 @@ const App = () => {
       {/*   <BoxObjectModelScreen /> */}
       {/*     <DimensionesScreen /> */}
       {/*       <PositionScreen /> */}
-      <FlexScreen />
+      {/*   <FlexScreen /> */}
+      <TareaScreen />
     </SafeAreaView>
   );
 };
