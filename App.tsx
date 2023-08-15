b857655f1c0c8f31b08bdb1ea960005202273092
Mauri <mauri.monzon91@gmail.com>
2023-08-14 22:50:02 -0300

    Flex

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import {PositionScreen} from './src/screens/PositionScreen';
 /* import {HolaMundoScreen} from './src/screens/HolaMundoScreen'; */
 /* import {ContadorScreen} from './src/screens/ContadorScreen'; */
 /* import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen'; */
 /* import {DimensionesScreen} from './src/screens/DimensionesScreen'; */
+/* import {PositionScreen} from './src/screens/PositionScreen'; */
+import {FlexScreen} from './src/screens/FlexScreen';
 
 const App = () => {
   return (
@@ -13,7 +14,8 @@ const App = () => {
       {/*     <ContadorScreen /> */}
       {/*   <BoxObjectModelScreen /> */}
       {/*     <DimensionesScreen /> */}
-      <PositionScreen />
+      {/*       <PositionScreen /> */}
+      <FlexScreen />
     </SafeAreaView>
   );
 };
